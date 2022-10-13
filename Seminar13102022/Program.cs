0bd87276ef04e3782aa82572258f1dfe8db5d9f9
Oleg-GH <fan2004g@bk.ru>
2022-10-13 13:04:50 +0500

    Квадрат числа, вводимого в консоли

---
+++ Seminar13102022/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+//Находим квадрат числа
+int sqr = number * number;
+Console.WriteLine($"Квадрат числа {number} равен {sqr}");
