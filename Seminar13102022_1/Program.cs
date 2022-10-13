4d493ab0f3a5d8669dc04a5748f72634ac6e293e
Oleg-GH <fan2004g@bk.ru>
2022-10-13 13:28:36 +0500

    Проверка квадрата числа

---
+++ Seminar13102022_1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("ВВедите число 1: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int numberB = int.Parse(Console.ReadLine());
+
+if(numberB / numberA == numberA)
+    {
+    Console.WriteLine($"{numberB} является квадратом числа {numberA}");
+    }
+else 
+    {
+    Console.WriteLine($"{numberB} не является квадратом числа {numberA}");
+    }
