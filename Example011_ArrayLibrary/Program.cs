14293a66662a5111c93996e19fda261ab3241b30
Oleg-GH <fan2004g@bk.ru>
2022-10-16 22:48:39 +0500

    Задача определения индекса искомого эл-та массива

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,48 @@
+﻿// Имеется одномерный массив array из n элементовю Требуется найти элемент массива, равный find.
+void FillArray(int[] collection)               // Метод заполнения массива collection 
+{                                              // случайными числами от 1 до 10   
+    int length = collection.Length;            // void - т.к. не выдается никакой результат
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)                     // Метод выдачи полученного массива на экран
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)        // Метод определения индекса искомого элемента в массиве
+{                                              // тут нет void, т.к. выдается некий результат
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];    // создай новый массив из 10 элементов. Изначально будут нули.
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
